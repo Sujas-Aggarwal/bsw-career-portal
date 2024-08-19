28d2b69e2c78c611303eb49181dd6d4190dceb8e
Sujas <ce1221047@iitd.ac.in>
2024-08-19 21:01:38 +0530

    svg enhance

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -33,6 +33,7 @@ function Carousel() {
     let images = [
         "https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&w=600",
         "https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.pexels.com/photos/257092/pexels-photo-257092.jpeg?auto=compress&cs=tinysrgb&w=600"
     ];
     const [currentImg, setCurrentImg] = useState<number>(0);
     return (
@@ -106,7 +107,7 @@ function LeftButton() {
                 x2="16.6374"
                 y2="0.799526"
                 stroke="black"
-                stroke-width="0.7"
+                strokeWidth="0.7"
             />
             <line
                 x1="1.27053"
@@ -114,7 +115,7 @@ function LeftButton() {
                 x2="16.9323"
                 y2="42.5992"
                 stroke="black"
-                stroke-width="0.7"
+                strokeWidth="0.7"
             />
         </svg>
     );
@@ -134,7 +135,7 @@ function RightButton() {
                 x2="1.28647"
                 y2="42.0213"
                 stroke="black"
-                stroke-width="0.7"
+                strokeWidth="0.7"
             />
             <line
                 x1="16.6533"
@@ -142,7 +143,7 @@ function RightButton() {
                 x2="0.991486"
                 y2="0.221584"
                 stroke="black"
-                stroke-width="0.7"
+                strokeWidth="0.7"
             />
         </svg>
     );
