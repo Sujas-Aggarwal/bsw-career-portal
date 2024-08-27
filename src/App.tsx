80dcbf0eba68a1cea94fd539553471115ce51c65
Sujas <ce1221047@iitd.ac.in>
2024-08-27 15:46:19 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Explore from "./components/Explore";
+
+export default function App() {
+    return (
+        <div >
+            <Navbar />
+            <Hero />
+            <Explore />
+        </div>
+    );
+}
+// function Gapdiv({ gap = 10 }: { gap?: number }) {
+//     return <div className={`h-${gap}`}></div>;
+// }
