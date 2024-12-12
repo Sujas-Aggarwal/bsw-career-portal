2aad82dc07f52c4affa20e827132469cc1b897ed
Sujas <ce1221047@iitd.ac.in>
2024-12-12 23:01:38 +0530

    vercel routing issue resolved

--- src/pages/Build.tsx
+++ src/pages/Build.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { setMetadata } from "../utils/seo";
+import Hero from "../components/explore/Hero";
 
 function Build() {
   const { profileName } = useParams();
@@ -11,7 +12,16 @@ function Build() {
       ["Build", profileName?.toUpperCase() ?? ""]
     );
   });
-  return <div>Build</div>;
+  return <div>
+    <Hero profileName={profileName} />
+    <Main profileName={profileName} />
+  </div>;
 }
-
+function Main({profileName}: {profileName: string | undefined}) {
+  return (
+    <div>
+      <h1>Build</h1>
+    </div>
+  );
+}
 export default Build;
